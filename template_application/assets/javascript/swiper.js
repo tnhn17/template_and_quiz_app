83122542f7203782af42e7e68a1d9d8fdd09990f
Tunahan Öztürk <tnhn17@windowslive.com>
2023-07-17 14:18:50 +0300

    deneme-yedekte kalsın

--- template_application/assets/javascript/swiper.js
+++ template_application/assets/javascript/swiper.js
@@ -1,21 +1,13 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
