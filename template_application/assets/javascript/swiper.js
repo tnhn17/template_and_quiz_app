baca3a0cc48bbaa01b192d973186ff3a5722653a
Tunahan Öztürk <tnhn17@windowslive.com>
2023-07-17 18:25:23 +0300

    update

--- template_application/assets/javascript/swiper.js
+++ template_application/assets/javascript/swiper.js
@@ -1,13 +1,40 @@
-var swiper = new Swiper(".slide-content", {
-  slidesPerView: 1,
-  spaceBetween: 30,
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 3,
+  spaceBetween: 20,
   loop: true,
+  centerSlide: "true",
+  grabCursor: "true",
+  fade:"true",
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    dynamicBullets: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+  
 });
